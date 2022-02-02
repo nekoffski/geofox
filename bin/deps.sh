57a52084aa11e952265eff7264144005999f2af9
nekoffski <kacperczyz@protonmail.com>
2022-02-02 18:58:12 +0100

    fix

--- bin/deps.sh
+++ bin/deps.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-sudo apt-get install build-essential
+sudo apt-get install -y build-essential
 sudo apt-get install -y python3.9
 sudo apt-get install -y python3-setuptools
 sudo apt-get install -y python3.9-venv
 sudo apt-get install -y python3.9-dev
+sudo apt-get install -y libffi-dev
 
 sudo python3.9 -m ensurepip
 sudo python3.9 -m pip install virtualenv
