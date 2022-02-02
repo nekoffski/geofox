2d8aeafdadb69d1c367a95d87650da201b94a3b9
nekoffski <kacperczyz@protonmail.com>
2022-02-02 18:37:39 +0100

    Add python dev tools to deps

--- bin/deps.sh
+++ bin/deps.sh
@@ -4,6 +4,7 @@ sudo apt-get install build-essential
 sudo apt-get install -y python3.9
 sudo apt-get install -y python3-setuptools
 sudo apt-get install -y python3.9-venv
+sudo apt-get install -y python3.9-dev
 
 sudo python3.9 -m ensurepip
 sudo python3.9 -m pip install virtualenv
