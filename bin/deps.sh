a43314656279da3d03d5e47fdcec6db571e1c77c
Kacper Czyż <kacperczyz@protonmail.com>
2022-02-03 19:00:42 +0100

    Update deps.sh

--- bin/deps.sh
+++ bin/deps.sh
@@ -4,5 +4,5 @@ sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt-get update
 
 sudo apt-get install -y build-essential tzdat musl-dev jpeg-dev zlib-dev libssl-dev libffi-dev cairo-dev pango-dev gdk-pixbuf-dev
-sudo apt-get install -y python3 python3-distutils python3-setuptools python3-venv python3-dev
+sudo apt-get install -y python3 python3-distutils python3-setuptools python3-venv python3-dev python3-wheel
 sudo apt-get install gcc-arm*
