fa26708c8faa2c7cb417166a8161f2a1640430db
nekoffski <kacperczyz@protonmail.com>
2022-02-03 17:49:30 +0100

    fix

--- bin/deps.sh
+++ bin/deps.sh
@@ -2,5 +2,5 @@
 
 sudo add-apt-repository -y ppa:deadsnakes/ppa 
 
-sudo apt-get install -y build-essential libffi-dev
+sudo apt-get install -y build-essential libffi-dev tzdat
 sudo apt-get install -y python3.8 python3.8-distutils python3-setuptools python3.8-venv python3.8-dev
