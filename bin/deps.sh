9e20aa591051c53181584c82d8198df13dd260ae
nekoffski <kacperczyz@protonmail.com>
2022-02-03 17:57:14 +0100

    fix

--- bin/deps.sh
+++ bin/deps.sh
@@ -4,3 +4,4 @@ sudo add-apt-repository -y ppa:deadsnakes/ppa
 
 sudo apt-get install -y build-essential libffi-dev tzdat
 sudo apt-get install -y python3.8 python3.8-distutils python3-setuptools python3.8-venv python3.8-dev
+sudo apt-get install gcc-arm*
