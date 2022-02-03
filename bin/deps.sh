f5ba216b86c49c303c86d62cab6aa4755fd7ead2
nekoffski <kacperczyz@protonmail.com>
2022-02-03 17:38:01 +0100

    fix

--- bin/deps.sh
+++ bin/deps.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y python3.8
 sudo apt-get install -y python3-setuptools
 sudo apt-get install -y python3.8-venv
 sudo apt-get install -y python3.8-dev
+sudo apt-get install -y python3.8-pip
 sudo apt-get install -y libffi-dev
 
-sudo python3.8 -m ensurepip
 sudo python3.8 -m pip install virtualenv
